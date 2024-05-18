7bae0fdac07018f7d427a5aaf97afe3067a603a3
kelvinehiz <kelvinehiz@gmail.com>
2024-05-18 11:04:51 +0000

    2-hbnb re route

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -1,49 +1,27 @@
 #!/usr/bin/python3
-"""
-Flask App that integrates with AirBnB static HTML Template
-"""
-import uuid
-from flask import Flask, render_template, url_for
+""" Starts a Flash Web Application """
 from models import storage
+from flask import Flask, render_template
+from api.v1.views import app_views
+from flask import jsonify
+from os import getenv
+from flask_cors import CORS
 
-# flask setup
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-port = 5000
-host = '0.0.0.0'
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
-
-# begin flask page rendering
 @app.teardown_appcontext
 def teardown_db(exception):
-    """
-    after each request, this method calls .close() (i.e. .remove()) on
-    the current SQLAlchemy Session
-    """
+    """ Closes storage on teardown """
     storage.close()
 
-
-@app.route('/2-hbnb/')
-def hbnb_filters(the_id=None):
-    """
-    handles request to custom template with states, cities & amentities
-    """
-    state_objs = storage.all('State').values()
-    states = dict([state.name, state] for state in state_objs)
-    amens = storage.all('Amenity').values()
-    places = storage.all('Place').values()
-    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
-                 for user in storage.all('User').values())
-    cache_id = (str(uuid.uuid4()))
-    return render_template('2-hbnb.html',
-                           states=states,
-                           amens=amens,
-                           places=places,
-                           users=users,
-                           cache_id=cache_id)
-
+@app.errorhandler(404)
+def not_found(error):
+    """ Handles 404 errors """
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
-    """
-    MAIN Flask App"""
-    app.run(host=host, port=port)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', '5003'))
+    app.run(host=host, port=port, threaded=True)
